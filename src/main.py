7cc6844af06c4318fae960f9d2f36b0b04d9b125
j-benit <146685640+j-benit@users.noreply.github.com>
2025-05-21 16:16:59 +0000

    update 2

--- src/main.py
+++ src/main.py
@@ -1,27 +1,30 @@
-# main.py
 from dataweb import extraer_laptops
 from database import DataBase
 import pandas as pd
-import os
 
 def main():
-    datos = extraer_laptops()
-    if not datos:
-        print("⚠️ No se extrajeron datos.")
-        return
+    try:
+        datos = extraer_laptops()
 
-    df = pd.DataFrame(datos)
+        if not datos:
+            print("No se extrajeron datos.")
+            return
 
-    # Guardar CSV
-    ruta_csv = "src/PAD_2025/src/static/csv/laptops.csv"
-    os.makedirs(os.path.dirname(ruta_csv), exist_ok=True)
-    df.to_csv(ruta_csv, index=False, encoding="utf-8")
-    print(f"✅ CSV guardado en: {ruta_csv}")
+        df = pd.DataFrame(datos)
+        print(df.head())
 
-    # Guardar en base de datos
-    db = DataBase()
-    db.guardar_df(df)
+        # Guardar en CSV
+        ruta_csv = "src/static/csv/laptops.csv"
+        df.to_csv(ruta_csv, index=False, encoding='utf-8')
+        print(f"✅ CSV guardado en: {ruta_csv}")
+
+        # Guardar en base de datos
+        db = DataBase()
+        db.guardar_df(df)
+
+    except Exception as e:
+        print("❌ Error al ejecutar extraer_laptops:")
+        print(e)
 
 if __name__ == "__main__":
     main()
-    

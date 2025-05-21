0f8c6feabce8efd4ab766c6b7e7a8c01048a9b1d
j-benit <146685640+j-benit@users.noreply.github.com>
2025-05-21 01:17:23 +0000

    main update

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from dataweb import extraer_laptops
 
 def main():
-    laptops = extraer_laptops()
-    for laptop in laptops:
-        print(f"Título: {laptop['titulo']}")
-        print(f"Precio: ${laptop['precio']}")
-        print("-" * 30)
+    try:
+        extraer_laptops()
+    except Exception as e:
+        print("❌ Error al ejecutar extraer_laptops:")
+        print(e)
 
 if __name__ == "__main__":
     main()

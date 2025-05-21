8aa4210937f769a1451f4a4d32ff3606f41c062a
j-benit <146685640+j-benit@users.noreply.github.com>
2025-05-21 16:31:29 +0000

    configurando extraccion de datos

--- src/main.py
+++ src/main.py
@@ -7,13 +7,13 @@ def main_1():
     dataweb = extraer_laptops()
     df = dataweb.obtener_datos() 
     df = dataweb.convertir_numericos(df) # capa 2 
-    df.to_csv("src/edu_pad/static/csv/data_extractor.csv", index=False)
+    df.to_csv("src/static/csv/data_extractor.csv", index=False)
 
     database = DataBase()
-    df = pd.read_csv("src/edu_pad/static/csv/data_extractor.csv")
+    df = pd.read_csv("src/static/csv/data_extractor.csv")
     df_db = database.guardar_df(df)
     df_db2 = database.obtener_datos() # capa 3 guardar 
-    df_db2.to_csv("src/edu_pad/static/csv/data_db.csv", index=False)
+    df_db2.to_csv("src/static/csv/data_db.csv", index=False)
 
 
 

1bec6b623a546ae9c3709a57bfd31b70f079ff2e
j-benit <146685640+j-benit@users.noreply.github.com>
2025-05-21 15:35:17 +0000

    intentadno crear el dtframe

--- src/dataweb.py
+++ src/dataweb.py
@@ -1,42 +1,79 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-import time
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import datetime
 
-def extraer_laptops():
-    # Configurar opciones de Chrome para el modo headless
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
 
-    # Inicializar el driver de Chrome
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-    # URL de búsqueda de laptops
-    url = "https://listado.mercadolibre.com.co/laptop#D[A:laptop]"
-    driver.get(url)
-    time.sleep(5)  # Esperar a que la página cargue
 
-    # Extraer los productos
-    productos = driver.find_elements(By.CLASS_NAME, "ui-search-result__wrapper")
+class DataWeb:
+    def __init__(self):
+        self.url = "https://listado.mercadolibre.com.co/laptop#D[A:laptop]"
+    
 
-    print(f"Productos encontrados: {len(productos)}")
-
-    for producto in productos:
+    def obtener_datos(self):
         try:
-            titulo = producto.find_element(By.CSS_SELECTOR, "h2, .ui-search-item__title").text
-            precio = producto.find_element(By.CSS_SELECTOR, ".andes-money-amount__fraction").text
-            print(f"Título: {titulo}")
-            print(f"Precio: ${precio}")
-            print("-" * 10)
-        except Exception as e:
-            print("Error al extraer producto:", e)
-            continue
+            # url , cabeceras
+            headers = {
+                'User-Agent': 'Mozilla/5.0'
+            }
+            respuesta = requests.get(self.url,headers=headers)
+            if respuesta.status_code != 200:
+                print("La url saco error, no respondio o no existe")
+            #print(respuesta.text)
+            soup = BeautifulSoup(respuesta.text,'html.parser')
+            tabla = soup.select_one('div[data-testid="history-table"] table')
+            nombre_columnas = [th.get_text(strip=True) for th in tabla.thead.find_all('th')]
+            filas = []
+            for tr in tabla.tbody.find_all('tr'):
+                columnas = [ td.get_text(strip=True) for td in tr.find_all('td')]
+                if len(columnas) == len(nombre_columnas):
+                    filas.append(columnas)
+            df = pd.DataFrame(filas,columns=nombre_columnas).rename(columns = {
+                'Fecha': 'fecha',
+                'Abrir': 'abrir',
+                'Máx.': 'max',
+                'Mín.': 'min',
+                'CerrarPrecio de cierre ajustado para splits.': 'cerrar',
+                'Cierre ajustadoPrecio de cierre ajustado para splits y distribuciones de dividendos o plusvalías.': 'cierre_ajustado',
+                'Volumen':'volumen'
+            })
+            df = self.convertir_numericos(df)
+            print("*******************************************************************")
+            print("Datos Obtenidos ")
+            print("*******************************************************************")
+            print(df.head())
+            #df.to_excel("dataweb_limpio.xlsx")
+            # print(nombre_columnas)
+            # print(filas)
+            # for th in tabla.thead.find_all('th'):
+            #     th_data = th.get_text(strip=True)
+# ['Fecha', 'Abrir', 'Máx.', 'Mín.', 'CerrarPrecio de cierre ajustado para splits.', 'Cierre ajustadoPrecio de cierre ajustado para splits y distribuciones de dividendos o plusvalías.', 'Volumen']
 
-    if productos:
-        print("Primer producto:", productos[0].text)
+            return df
+        except Exception as err:
+            print("Error en la funcion obtener_datos")
+            df = pd.DataFrame()
 
-    driver.quit()
+
+    def convertir_numericos(self,df=pd.DataFrame()):
+        df= df.copy()
+        if len(df)>0:
+            #for col in (df.columns):
+            for col in ('abrir',	'max',	'min',	'cerrar',	'cierre_ajustado',	'volumen'):
+                df[col] = (df[col]
+                           .str.replace(r"\.","",regex=True)
+                           .str.replace(",",'.'))
+
+        return df
+
+
+
+
+# dw = DataWeb()
+# dw.obtener_datos()
+# #print(dw.url)
+
+# nom_columas = ["kjakl","kjkhasd","jklhasd"]
+# columna =     [4654,    6456,     465456]
+# fila =       [[4654,    6456,     465456],[4654,    6456,     465456],[4654,    6456,     465456]]

ded945ec00a1c3503bf971ba68342c8844fd59f6
j-benit <146685640+j-benit@users.noreply.github.com>
2025-05-21 16:03:39 +0000

    main.py actualizado

--- src/main.py
+++ src/main.py
@@ -1,30 +1,26 @@
+# main.py
 from dataweb import extraer_laptops
 from database import DataBase
 import pandas as pd
+import os
 
 def main():
-    try:
-        datos = extraer_laptops()
+    datos = extraer_laptops()
+    if not datos:
+        print("⚠️ No se extrajeron datos.")
+        return
 
-        if not datos:
-            print("No se extrajeron datos.")
-            return
+    df = pd.DataFrame(datos)
 
-        df = pd.DataFrame(datos)
-        print(df.head())
+    # Guardar CSV
+    ruta_csv = "src/PAD_2025/src/static/csv/laptops.csv"
+    os.makedirs(os.path.dirname(ruta_csv), exist_ok=True)
+    df.to_csv(ruta_csv, index=False, encoding="utf-8")
+    print(f"✅ CSV guardado en: {ruta_csv}")
 
-        # Guardar en CSV
-        ruta_csv = "src/PAD_2025/src/static/csv/laptops.csv"
-        df.to_csv(ruta_csv, index=False, encoding='utf-8')
-        print(f"✅ CSV guardado en: {ruta_csv}")
-
-        # Guardar en base de datos
-        db = DataBase()
-        db.guardar_df(df)
-
-    except Exception as e:
-        print("❌ Error al ejecutar extraer_laptops:")
-        print(e)
+    # Guardar en base de datos
+    db = DataBase()
+    db.guardar_df(df)
 
 if __name__ == "__main__":
     main()

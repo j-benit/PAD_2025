cdfd5af83080d8b446c3b21be6dc5aad21d6353a
j-benit <146685640+j-benit@users.noreply.github.com>
2025-05-21 16:25:19 +0000

    atos

--- src/main.py
+++ src/main.py
@@ -2,29 +2,21 @@ from dataweb import extraer_laptops
 from database import DataBase
 import pandas as pd
 
-def main():
-    try:
-        datos = extraer_laptops()
 
-        if not datos:
-            print("No se extrajeron datos.")
-            return
+def main_1():
+    dataweb = extraer_laptops()
+    df = dataweb.obtener_datos() 
+    df = dataweb.convertir_numericos(df) # capa 2 
+    df.to_csv("src/edu_pad/static/csv/data_extractor.csv", index=False)
 
-        df = pd.DataFrame(datos)
-        print(df.head())
+    database = DataBase()
+    df = pd.read_csv("src/edu_pad/static/csv/data_extractor.csv")
+    df_db = database.guardar_df(df)
+    df_db2 = database.obtener_datos() # capa 3 guardar 
+    df_db2.to_csv("src/edu_pad/static/csv/data_db.csv", index=False)
 
-        # Guardar en CSV
-        ruta_csv = "src/static/csv/laptops.csv"
-        df.to_csv(ruta_csv, index=False, encoding='utf-8')
-        print(f"✅ CSV guardado en: {ruta_csv}")
 
-        # Guardar en base de datos
-        db = DataBase()
-        db.guardar_df(df)
 
-    except Exception as e:
-        print("❌ Error al ejecutar extraer_laptops:")
-        print(e)
 
 if __name__ == "__main__":
-    main()
+    main_1()
